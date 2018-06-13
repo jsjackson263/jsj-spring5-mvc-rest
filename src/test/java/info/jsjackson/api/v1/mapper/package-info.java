1f17e18ea6a459637ede61aa87f19d301b32df83
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-06-13 22:38:14 +1200

    update package info doc

--- src/test/java/info/jsjackson/api/v1/mapper/package-info.java
+++ src/test/java/info/jsjackson/api/v1/mapper/package-info.java
@@ -1,6 +1,6 @@
 /**
- *  The tests in this package will fail because these tests are being run against classes generated 
- *  from the MapStruct library, and the classes are in target, not in the class path.
+ *  If using Eclipse, the tests in this package will fail because these tests are being run against 
+ *  classes generated from the MapStruct library, and the classes are in target, not in the class path.
  *  To add the classes to the class path, do the following (in Eclipse):
  *  - Build Path -> Configure Build Path 
  *  - Source -> Add Folder -> target/generated-sources/annotations
