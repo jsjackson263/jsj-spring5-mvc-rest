f8193a2b8f5cd25b871a37ff526fa94e8959302b
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-06-18 19:24:35 +1200

    update CustomerDTO: lombok annotations, @JsonProperty for customerUrl

--- src/main/java/info/jsjackson/api/v1/model/CustomerDTO.java
+++ src/main/java/info/jsjackson/api/v1/model/CustomerDTO.java
@@ -3,18 +3,28 @@
  */
 package info.jsjackson.api.v1.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author jsjackson
  *
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDTO {
 	
 	private Long id;
 	private String firstName;
 	private String lastName;
+	
+	@JsonProperty("customer_url")
 	private String customerUrl;
 
 }
