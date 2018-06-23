ebb20c8b6ddaaff6ef1c2f3f8c5a760c363b746d
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-06-23 19:04:09 +1200

    add new class VendorListDTO

---
+++ src/main/java/info/jsjackson/api/v1/model/VendorListDTO.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package info.jsjackson.api.v1.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author jsjackson
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VendorListDTO {
+
+	List<VendorDTO> vendors;
+}
