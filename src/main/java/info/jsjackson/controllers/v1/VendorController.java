b041b1a34c31be1148f1bbf8b68529b65fcad18c
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-06-24 15:43:30 +1200

    implement VendorController operations, + tests

---
+++ src/main/java/info/jsjackson/controllers/v1/VendorController.java
@@ -0,0 +1,111 @@
+/**
+ * 
+ */
+package info.jsjackson.controllers.v1;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import info.jsjackson.api.v1.model.VendorDTO;
+import info.jsjackson.api.v1.model.VendorListDTO;
+import info.jsjackson.services.VendorService;
+
+/**
+ * @author jsjackson
+ *
+ */
+@RestController
+@RequestMapping(VendorController.BASE_URL)
+public class VendorController {
+
+	public static final String BASE_URL = "/api/v1/vendors";
+	
+	private final VendorService vendorService;
+	
+	/**
+	 * @param vendorService
+	 */
+	public VendorController(VendorService vendorService) {
+		this.vendorService = vendorService;
+	}
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public VendorListDTO getAllVendors() {
+		
+		List<VendorDTO>  vendorList = vendorService.getAllVendors();
+		
+		VendorListDTO vendorListDTO = new VendorListDTO(vendorList);
+		
+		return vendorListDTO;
+		
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDTO getVendorById(@PathVariable String id) {
+		
+		VendorDTO vendorDTO = vendorService.getVendorById(Long.valueOf(id));
+		
+		return vendorDTO;
+	}
+	
+	@GetMapping("/vendor/{name}")
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDTO getVendorByName(@PathVariable String name) {
+		
+		VendorDTO vendorDTO = vendorService.getVendorByName(name);
+		
+		return vendorDTO;
+		
+	}
+	
+	
+	@PostMapping()
+	@ResponseStatus(HttpStatus.CREATED)
+	public VendorDTO createNewVendor(@RequestBody VendorDTO vendorDTO) {
+		
+		VendorDTO returnedDTO = vendorService.createNewVendor(vendorDTO);
+		
+		return returnedDTO;
+	}
+	
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDTO updateVendor(@PathVariable String id, @RequestBody VendorDTO vendorDTO) {
+		
+		VendorDTO returnedDTO = vendorService.saveVendorByDTO(Long.valueOf(id), vendorDTO);
+		
+		return returnedDTO;
+		
+	}
+	
+	@PatchMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDTO patchVendor(@PathVariable String id, @RequestBody VendorDTO vendorDTO) {
+		
+		VendorDTO returnedDTO = vendorService.patchVendor(Long.valueOf(id), vendorDTO);
+		
+		return returnedDTO;
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteVendorById(@PathVariable String id) {
+		
+		vendorService.deleteVendor(Long.valueOf(id));
+		
+	}
+}
---
+++ src/test/java/info/jsjackson/controllers/v1/VendorControllerTest.java
@@ -0,0 +1,281 @@
+/**
+ * 
+ */
+package info.jsjackson.controllers.v1;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import info.jsjackson.api.v1.model.VendorDTO;
+import info.jsjackson.services.VendorService;
+
+/**
+ * @author jsjackson
+ *
+ */
+public class VendorControllerTest extends AbstractRestControllerTest {
+
+	public static final Long ID1 = 1L;
+	public static final String NAME1 = "Tutti Frutti";
+	public static final String URL1 = VendorController.BASE_URL + "/" + ID1;
+
+	public static final Long ID2 = 2L;
+	public static final String NAME2 = "Fruits R Us";
+	public static final String URL2 = VendorController.BASE_URL + "/" + ID2;
+	
+	public static final Long ID3 = 3L;
+	public static final String NAME3 = "New Vendor";
+	public static final String URL3 = VendorController.BASE_URL + "/" + ID3;
+	
+	
+	@InjectMocks
+	VendorController vendorController;
+
+	@Mock
+	VendorService vendorService;
+	
+	MockMvc mockMvc;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		
+		mockMvc = MockMvcBuilders.standaloneSetup(vendorController).build();
+	}
+
+	@Test
+	public void testGetAllVendors() throws Exception  {
+
+		//Given
+		List<VendorDTO> vendorList = new ArrayList<>();
+		
+		VendorDTO vendorDTO1 = new VendorDTO();
+		vendorDTO1.setId(ID1);
+		vendorDTO1.setName(NAME1);
+		vendorDTO1.setVendorUrl(URL1);
+		vendorList.add(vendorDTO1);
+		
+		VendorDTO vendorDTO2 = new VendorDTO();
+		vendorDTO2.setId(ID2);
+		vendorDTO2.setName(NAME2);
+		vendorDTO2.setVendorUrl(URL2);
+		vendorList.add(vendorDTO2);
+		
+		when(vendorService.getAllVendors()).thenReturn(vendorList);
+
+		
+		//When/Then
+		mockMvc.perform(get(vendorController.BASE_URL)
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.vendors", hasSize(2)))
+		.andReturn();
+		
+	
+	}
+	
+	@Test
+	public void testGetVendorById() throws Exception  {
+
+		//Given
+		
+		VendorDTO vendorDTO1 = new VendorDTO();
+		vendorDTO1.setId(ID1);
+		vendorDTO1.setName(NAME1);
+		vendorDTO1.setVendorUrl(URL1);
+		
+		when(vendorService.getVendorById(anyLong())).thenReturn(vendorDTO1);
+
+		
+		//When/Then
+		mockMvc.perform(get(VendorController.BASE_URL + "/" + ID1)
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.name", equalTo(NAME1)))
+		.andReturn();
+		
+	
+	}
+	
+	@Test
+	public void testGetVendorByName() throws Exception  {
+
+		//Given
+		
+		VendorDTO vendorDTO1 = new VendorDTO();
+		vendorDTO1.setId(ID1);
+		vendorDTO1.setName(NAME1);
+		vendorDTO1.setVendorUrl(URL1);
+		
+		when(vendorService.getVendorByName(anyString())).thenReturn(vendorDTO1);
+
+		
+		//When/Then
+		mockMvc.perform(get(VendorController.BASE_URL + "/vendor/" + NAME1)
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.name", equalTo(NAME1)))
+		.andReturn();
+		
+	
+	}
+	
+	@Test
+	public void testGetVendorByNameDEBUG() throws Exception  {
+
+		//Given
+		
+		VendorDTO vendorDTO1 = new VendorDTO();
+		vendorDTO1.setId(ID1);
+		vendorDTO1.setName(NAME1);
+		vendorDTO1.setVendorUrl(URL1);
+		
+		when(vendorService.getVendorByName(anyString())).thenReturn(vendorDTO1);
+
+		
+		//When/Then
+		String response = mockMvc.perform(get(VendorController.BASE_URL + "/vendor/" + NAME1)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(vendorDTO1)))
+		.andReturn().getResponse().getContentAsString();
+			
+		System.out.println("Response: " + response);
+	
+	}
+	
+	
+	@Test
+	public void testCreateNewVendor() throws Exception  {
+
+		//Given
+		VendorDTO vendorDTO3 = new VendorDTO();
+		vendorDTO3.setId(ID3);
+		vendorDTO3.setName(NAME3);
+		vendorDTO3.setVendorUrl(URL3);
+		
+		VendorDTO returnedDTO = new VendorDTO();
+		returnedDTO.setId(vendorDTO3.getId());
+		returnedDTO.setName(vendorDTO3.getName());
+		returnedDTO.setVendorUrl(vendorDTO3.getVendorUrl());
+		
+		when(vendorService.createNewVendor(vendorDTO3)).thenReturn(returnedDTO);
+
+		
+		//When/Then
+		mockMvc.perform(post(VendorController.BASE_URL)
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(vendorDTO3)))
+		.andExpect(status().isCreated())
+		.andExpect(jsonPath("$.name", equalTo("New Vendor")))
+		.andExpect(jsonPath("$.vendorUrl", equalTo("/api/v1/vendors/3")))
+		.andReturn();
+		
+	}
+	
+	@Test
+	public void testUpdateVendor() throws Exception  {
+
+		//Given
+		VendorDTO vendorDTO3 = new VendorDTO();
+		vendorDTO3.setId(ID3);
+		vendorDTO3.setName(NAME3);
+		vendorDTO3.setVendorUrl(URL3);
+		
+		VendorDTO returnedDTO = new VendorDTO();
+		returnedDTO.setId(vendorDTO3.getId());
+		returnedDTO.setName(vendorDTO3.getName());
+		returnedDTO.setVendorUrl(vendorDTO3.getVendorUrl());
+		
+		when(vendorService.saveVendorByDTO(ID3, vendorDTO3)).thenReturn(returnedDTO);
+
+		
+		//When/Then
+		mockMvc.perform(put(VendorController.BASE_URL + "/" + ID3)
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(vendorDTO3)))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.name", equalTo("New Vendor")))
+		.andExpect(jsonPath("$.vendorUrl", equalTo("/api/v1/vendors/3")))
+		.andReturn();
+		
+	}
+	
+
+	@Test
+	public void testPatchVendor() throws Exception  {
+
+		//Given
+		VendorDTO vendorDTO3 = new VendorDTO();
+		vendorDTO3.setId(ID3);
+		vendorDTO3.setName(NAME3);
+		vendorDTO3.setVendorUrl(URL3);
+		
+		VendorDTO returnedDTO = new VendorDTO();
+		returnedDTO.setId(vendorDTO3.getId());
+		returnedDTO.setName("New Patched Vendor Name");
+		returnedDTO.setVendorUrl(vendorDTO3.getVendorUrl());
+		
+		when(vendorService.patchVendor(ID3, vendorDTO3)).thenReturn(returnedDTO);
+
+		
+		//When/Then
+		mockMvc.perform(patch(VendorController.BASE_URL + "/" + ID3)
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(vendorDTO3)))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.name", equalTo("New Patched Vendor Name")))
+		.andExpect(jsonPath("$.vendorUrl", equalTo("/api/v1/vendors/3")))
+		.andReturn();
+		
+	}
+	
+	@Test
+	public void testDeleteVendorById() throws Exception  {
+
+		//Given
+		Long idToDelete = ID3;
+		
+		//When/Then
+		mockMvc.perform(delete(VendorController.BASE_URL + "/" + idToDelete)
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andReturn();
+		
+		verify(vendorService, times(1)).deleteVendor(anyLong());
+		
+	}
+	
+	
+}
